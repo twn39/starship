4797738dab0b050cd8985cd34153ba5bd642d7bc
curry tang <curry_tang@apple.com>
2024-07-22 11:37:29 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import gradio as gr
-from gradio import FileData
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from llm import DeepSeekLLM, OpenRouterLLM, TongYiLLM
 from config import settings
 import base64
@@ -58,6 +57,58 @@ def update_chat(_provider: str, _chat, _model: str, _temperature: float, _max_to
     return _chat
 
 
+def explain_code(_code_type: str, _code: str, _chat):
+    if _chat is None:
+        _chat = init_chat()
+    chat_messages = [
+        SystemMessage(content=f'你的任务是获取提供的代码片段，并用简单易懂的语言解释它。分解代码的功能、目的和关键组件。使用类比、示例和通俗术语，使解释对编码知识很少的人来说易于理解。除非绝对必要，否则避免使用技术术语，并为使用的任何术语提供清晰的解释。目标是帮助读者在高层次上理解代码的作用和工作原理。'),
+        HumanMessage(content=_code),
+    ]
+    response_message = ''
+    for chunk in _chat.stream(chat_messages):
+        response_message = response_message + chunk.content
+        yield response_message
+
+
+def optimize_code(_code_type: str, _code: str, _chat):
+    if _chat is None:
+        _chat = init_chat()
+    chat_messages = [
+        SystemMessage(content=f'你的任务是分析提供的 {_code_type} 代码片段，并提出改进建议以优化其性能。确定可以使代码更高效、更快或更节省资源的地方。提供具体的优化建议，并解释这些更改如何提高代码的性能。优化后的代码应该保持与原始代码相同的功能，同时展示出更高的效率。'),
+        HumanMessage(content=_code),
+    ]
+    response_message = ''
+    for chunk in _chat.stream(chat_messages):
+        response_message = response_message + chunk.content
+        yield response_message
+
+
+def debug_code(_code_type: str, _code: str, _chat):
+    if _chat is None:
+        _chat = init_chat()
+    chat_messages = [
+        SystemMessage(content=f'你的任务是分析提供的 {_code_type} 代码片段，识别其中存在的任何错误，并提供一个修正后的代码版本来解决这些问题。解释你在原始代码中发现的问题以及你的修复如何解决它们。修正后的代码应该是功能性的、高效的，并遵循 {_code_type} 编程的最佳实践。'),
+        HumanMessage(content=_code),
+    ]
+    response_message = ''
+    for chunk in _chat.stream(chat_messages):
+        response_message = response_message + chunk.content
+        yield response_message
+
+
+def function_gen(_code_type: str, _code: str, _chat):
+    if _chat is None:
+        _chat = init_chat()
+    chat_messages = [
+        SystemMessage(content=f'你的任务是根据提供的自然语言请求创建 {_code_type} 函数。这些请求将描述函数的期望功能，包括输入参数和预期返回值。根据给定的规范实现这些函数，确保它们能够处理边缘情况，执行必要的验证，并遵循 {_code_type} 编程的最佳实践。请在代码中包含适当的注释，以解释逻辑并帮助其他开发人员理解实现。'),
+        HumanMessage(content=_code),
+    ]
+    response_message = ''
+    for chunk in _chat.stream(chat_messages):
+        response_message = response_message + chunk.content
+        yield response_message
+
+
 with gr.Blocks() as app:
     with gr.Tab('聊天'):
         chat_engine = gr.State(value=None)
@@ -71,7 +122,7 @@ with gr.Blocks() as app:
                     additional_inputs=[chat_engine]
                 )
             with gr.Column(scale=1, min_width=300):
-                with gr.Accordion('参数设置', open=True):
+                with gr.Accordion('模型参数设置', open=True):
                     with gr.Column():
                         provider = gr.Dropdown(
                             label='模型厂商',
@@ -124,6 +175,29 @@ with gr.Blocks() as app:
                             inputs=[provider, chat_engine, model, temperature, max_tokens],
                             outputs=[chat_engine],
                         )
+
+    with gr.Tab('代码优化'):
+        chat_engine = gr.State(value=None)
+        with gr.Row():
+            with gr.Column(scale=1):
+                with gr.Row(variant="panel"):
+                    code_result = gr.Markdown(label='解释结果', value=None)
+            with gr.Column(scale=1):
+                code_type = gr.Dropdown(
+                    label='代码类型',
+                    choices=['Javascript', 'Typescript', 'Python', 'C++', 'PHP', 'Java'],
+                    value='Javascript',
+                )
+                code = gr.Textbox(label='代码', lines=10, value=None)
+                with gr.Row():
+                    explain_code_btn = gr.Button('解释代码')
+                    optimize_code_btn = gr.Button('优化代码')
+                    debug_code_btn = gr.Button('错误修复')
+                    function_gen_btn = gr.Button('函数生成')
+            explain_code_btn.click(fn=explain_code, inputs=[code_type, code, chat_engine], outputs=[code_result])
+            optimize_code_btn.click(fn=optimize_code, inputs=[code_type, code, chat_engine], outputs=[code_result])
+            debug_code_btn.click(fn=debug_code, inputs=[code_type, code, chat_engine], outputs=[code_result])
+            function_gen_btn.click(fn=function_gen, inputs=[code_type, code, chat_engine], outputs=[code_result])
 
 
 app.launch(debug=settings.debug, show_api=False)
